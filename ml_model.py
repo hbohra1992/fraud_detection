b17473cf56571bae99ee70fa7c24f5730ca5b8a7
hbohra1992 <sunny01071992@gmail.com>
2022-10-26 23:20:41 +0530

    Added New ML Model Code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,15 @@
 print("adding all ML Related Code Here")
+
+def model1():
+    print("Adding Code to model 1")
+
+def model2():
+    print("Adding Code to model 2")
+
+
+def model3():
+    print("Adding Code to model 3")
+
+
+def model4():
+    print("Adding Code to model 4")            
